5fefbb9a9e2ee576de7fdccf3b3957e5c386a9ac
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-03-14 23:39:10 +0900

    課題提出練習

---
+++ hasegawatakuto/src/hasegawatakuto/Hasegawatakuto.java
@@ -0,0 +1,13 @@
+package hasegawatakuto;
+
+public class Hasegawatakuto {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}private void sysout() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
