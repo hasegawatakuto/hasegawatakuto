ac7394c8c8650bfd9f128221a432c1a094dab181
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-03-14 23:50:24 +0900

    練習2修正

--- hasegawatakuto/src/hasegawatakuto/Hasegawatakuto.java
+++ hasegawatakuto/src/hasegawatakuto/Hasegawatakuto.java
@@ -4,10 +4,7 @@ public class Hasegawatakuto {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-	}private void sysout() {
-		// TODO 自動生成されたメソッド・スタブ
-
+System.out.println("1");
 	}
 
 }
